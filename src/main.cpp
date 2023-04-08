de519459bc976a3bcadef76811708747659f82d2
EddieBreeg <eddiebreeg0@protonmail.com>
2023-04-08 11:20:47 -0400

    updated gitignore

--- src/main.cpp
+++ src/main.cpp
@@ -4,14 +4,11 @@ class Vaultware: public wxApp
 {
 	wxFrame* _mainWin = nullptr;
 public:
-	Vaultware() = default;
-	~Vaultware() = default;
 	virtual bool OnInit() override {
 		_mainWin = new wxFrame(nullptr, wxID_ANY, "Vaultware");
 		return _mainWin->Show();
 	}
 
 };
-
 
 wxIMPLEMENT_APP(Vaultware);

218df39647b75fc8cb9db9218a8b149ac3e85c76
EddieBreeg <eddiebreeg0@protonmail.com>
2023-04-16 18:34:22 -0400

    fixed double call to login

--- src/main.cpp
+++ src/main.cpp
@@ -29,7 +29,6 @@ public:
 		_loginPanel = new LoginPanel(_mainWin);
 		if (_loginPanel->ShowModal() == wxID_OK) {
 			std::string email(_loginPanel->GetLogin()), pwd(_loginPanel->GetPassword());
-			DEBUG_LOG(_vault.login(email, pwd) << '\n');
 			if (_vault.login(email, pwd))
 				_loginPanel->Destroy();
 			else

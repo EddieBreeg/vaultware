130f8b3a3daecd322d4f307e78033cb243185f2e
EddieBreeg <eddiebreeg0@protonmail.com>
2023-04-17 12:50:50 -0400

    password generator decorator class

--- include/PasswordGenerator.hpp
+++ include/PasswordGenerator.hpp
@@ -6,28 +6,33 @@
 #include <random>
 
 template<class PRNG>
-std::string generatePassword(PRNG& g, size_t len, bool digits, size_t minSpecials){
-    static constexpr char symbols[] = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
-    "$@.?!*%";
-    std::string pwd;
-    size_t start = int(!digits) * 10, end = 61 + 7 * int(minSpecials > 0);
-    pwd.reserve(len);
-    minSpecials = minSpecials < len ? minSpecials : len;
-    std::uniform_int_distribution<size_t> dist{start, end};
-    size_t s = 0;
-    for(size_t i = 0; i < len; ++i){
-        int j = dist(g);
-        s += j > 62;
-        pwd.push_back(symbols[dist(g)]);
+class PasswordGenerator{
+    PRNG& _g;
+public:
+    PasswordGenerator(PRNG& prng): _g(prng) {}
+    std::string operator()(size_t len, bool digits, size_t minSpecials){
+        static constexpr char symbols[] = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+        "$@.?!*%";
+        std::string pwd;
+        size_t start = int(!digits) * 10, end = 61 + 7 * int(minSpecials > 0);
+        pwd.reserve(len);
+        minSpecials = minSpecials < len ? minSpecials : len;
+        std::uniform_int_distribution<size_t> dist{start, end};
+        size_t s = 0;
+        for(size_t i = 0; i < len; ++i){
+            int j = dist(_g);
+            s += j > 62;
+            pwd.push_back(symbols[dist(_g)]);
+        }
+        dist = decltype(dist){62, 68};
+        while(s < minSpecials){
+            size_t i  = decltype(dist){0, len-1}(_g);
+            if(!isalnum(pwd[i])) continue;
+            pwd[i] = symbols[dist(_g)];
+            ++s;
+        }
+        return pwd;
     }
-    dist = decltype(dist){62, 68};
-    while(s < minSpecials){
-        size_t i  = decltype(dist){0, len-1}(g);
-        if(!isalnum(pwd[i])) continue;
-        pwd[i] = symbols[dist(g)];
-        ++s;
-    }
-    return pwd;
-}
+};
 
 #endif
